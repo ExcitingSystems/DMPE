b2a49a4f83e18b2ce713f6c13cbd7657a9e41ee2
Hendrik Vater <hvater@mail.uni-paderborn.de>
2024-04-23 17:10:40 +0200

    added specific function for simulation with environments

--- model_utils.py
+++ model_utils.py
@@ -2,49 +2,89 @@ from functools import partial
 
 import jax
 import jax.numpy as jnp
+import equinox as eqx
+from models import NeuralEulerODE
 
-import exciting_environments as excenvs
 
-
-@partial(jax.jit, static_argnums=(0, 1))
+@eqx.filter_jit
 def simulate_ahead(
-    model: excenvs.core_env.CoreEnvironment,  # typehint for the time being...
-    n_steps: int,
+    model: NeuralEulerODE,
     init_obs: jnp.ndarray,
-    init_state: jnp.ndarray,
     actions: jnp.ndarray,
-    env_state_normalizer,
-    action_normalizer,
-    static_params
+    tau: float
 ) -> jnp.ndarray:
     """Uses the given model to look ahead and simulate future observations.
     
     Args:
         model: The model to use in the simulation
-        n_steps: The number of steps to simulate into the future
-        obs: The current observations from which to start the simulation
-        state: The current state from which to start the simulation
-        actions: The actions to apply in each step of the simulation
+        init_obs: The initial observation from which to start the simulation
+        actions: The actions to apply in each step of the simulation, the length
+            of the first dimension of this array determine the lenght of the
+            output.
+        tau: The sampling time for the model
 
     Returns:
-        observations: The gathered observations
+        observations: The gathered observations. The shape of this is given as
+            (n_actions + 1, obs_dim). That is because the first observation is
+            already given through the initial observation
+    """
+
+    def body_fun(carry, action):
+        obs = carry
+        obs = model.step(obs, action, tau)
+        return obs
+
+    _, observations = jax.lax.scan(body_fun, init_obs, actions)
+    observations = jnp.concatenate([init_obs[None, :], observations], axis=0)
+
+    return observations
+
+
+@partial(jax.jit, static_argnums=(0))
+def simulate_ahead_with_env(
+    env,
+    init_obs: jnp.ndarray,
+    init_state: jnp.ndarray,
+    actions: jnp.ndarray,
+    env_state_normalizer: jnp.ndarray,
+    action_normalizer: jnp.ndarray,
+    static_params: dict
+) -> jnp.ndarray:
+    """Uses the given environment to look ahead and simulate future observations.
+    This is used to have perfect predictions
+
+    Args:
+        model: The model to use in the simulation
+        init_obs: The initial observation from which to start the simulation
+        init_state: The initial state from which to start the simulation
+        actions: The actions to apply in each step of the simulation, the length
+            of the first dimension of this array determine the lenght of the
+            output.
+        env_state_normalizer: Values for state normalization
+        action_normalizer: Values for action normalization
+        static_params: Static parameters of the environment
+
+    Returns:
+        observations: The gathered observations. The shape of this is given as
+            (n_actions + 1, obs_dim). That is because the first observation is
+            already given through the initial observation
     """
 
     def body_fun(carry, action):
         obs, state = carry
 
-        state = model._ode_exp_euler_step(
+        state = env._ode_exp_euler_step(
             state,
             action,
             env_state_normalizer,
             action_normalizer,
             static_params
         )
-        next_obs = model.generate_observation(state)
+        obs = env.generate_observation(state)
 
-        return (obs, state), next_obs
+        return (obs, state), obs
 
-    (obs, state), observations = jax.lax.scan(body_fun, (init_obs, init_state), actions[:-1, :])
-    observations = jnp.concatenate([obs[None, :], observations], axis=0)
+    (_, _), observations = jax.lax.scan(body_fun, (init_obs, init_state), actions)
+    observations = jnp.concatenate([init_obs[None, :], observations], axis=0)
 
     return observations
