fixed mistake in JSDLoss definition and added normalization to JSDLoss in optimization

exciting_exciting_systems/excitation/excitation_utils.py
@@ -62,9 +62,12 @@ def loss_function(
         observations = simulate_ahead(model=model, init_obs=init_obs, actions=actions, tau=tau)
 
     predicted_density_estimate = update_density_estimate_multiple_observations(
-        density_estimate, jnp.concatenate([observations[0:-1, :], actions], axis=-1)  # observations
+        density_estimate, observations  # density_estimate, jnp.concatenate([observations[0:-1, :], actions], axis=-1)
     )
-    loss = JSDLoss(p=predicted_density_estimate.p, q=target_distribution)
+    loss = JSDLoss(
+        p=predicted_density_estimate.p / jnp.sum(predicted_density_estimate.p),
+        q=target_distribution / jnp.sum(target_distribution),
+    )
 
     # TODO: pull this automatically, maybe penalty_kwargs or something
     rho_obs = 1e4
@@ -167,7 +170,7 @@ class Exciter(eqx.Module):
             in_axes=[DensityEstimate(0, None, None, None), 0],
             out_axes=DensityEstimate(0, None, None, None),
         )(
-            density_estimate, jnp.concatenate([obs, action], axis=-1)  # obs
+            density_estimate, obs  # density_estimate, jnp.concatenate([obs, action], axis=-1)
         )
 
         return action, next_proposed_actions, density_estimate

exciting_exciting_systems/utils/metrics.py
@@ -38,7 +38,8 @@ def jensen_shannon_divergence(p: jnp.ndarray, q: jnp.ndarray):
     assert p.shape == q.shape, "The two inputs need to be of the same shape."
     assert p.shape[-1] == q.shape[-1] == 1, "Last dim needs to be of length 1 for PDFs"
 
-    return (kullback_leibler_divergence(p, q) + kullback_leibler_divergence(q, p)) / 2
+    m = (p + q) / 2
+    return (kullback_leibler_divergence(p, m) + kullback_leibler_divergence(q, m)) / 2
 
 
 @jax.jit