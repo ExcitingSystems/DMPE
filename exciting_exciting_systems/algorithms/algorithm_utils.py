d6a33ad27c1547c532e9ce22d6c8e13b503bf2b8
Hendrik Vater <vater@lea.uni-paderborn.de>
2024-06-21 14:41:51 +0200

    updated env interaction with new functionalites in excenvs

--- exciting_exciting_systems/algorithms/algorithm_utils.py
+++ exciting_exciting_systems/algorithms/algorithm_utils.py
@@ -6,7 +6,7 @@ def interact_and_observe(env, k, action, obs, state, actions, observations):
     """Interact with the environment and store the resulting effects."""
 
     # apply u_k = \hat{u}_{k+1} and go to x_{k+1}
-    obs, _, _, _, state = env.step(state, action * env.env_properties.action_constraints.torque, env.env_properties)
+    obs, _, _, _, state = env.step(state, action, env.env_properties)
 
     actions = actions.at[k].set(action)  # store u_k
     observations = observations.at[k + 1].set(obs)  # store x_{k+1}
