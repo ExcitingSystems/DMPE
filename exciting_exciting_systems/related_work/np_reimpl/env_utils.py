5a14b3fc6257a643db3681497279599fa9f94abb
Hendrik Vater <vater@lea.uni-paderborn.de>
2024-07-10 14:58:26 +0200

    changed Pendulum API to be compatible with exciting environments

--- exciting_exciting_systems/related_work/np_reimpl/env_utils.py
+++ exciting_exciting_systems/related_work/np_reimpl/env_utils.py
@@ -5,12 +5,8 @@ def simulate_ahead_with_env(env, obs, state, actions):
     observations = []
     observations.append(obs)
 
-    state = state[None, ...]
-
     for i in range(actions.shape[0]):
-        state = env.step(state, actions[None, i, :])
-        obs = env.generate_observation(state)
+        obs, _, _, _, state = env.step(state, actions[i, :], env.env_properties)
+        observations.append(obs)
 
-        observations.append(obs[0])
-
-    return np.vstack(observations), state[0]
+    return np.vstack(observations), state
