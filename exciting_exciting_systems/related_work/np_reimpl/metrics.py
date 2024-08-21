622a4cd7d381a51da71fbdf9604d02a5e0930cb5
Hendrik Vater <vater@lea.uni-paderborn.de>
2024-08-21 10:16:14 +0200

    updated numpy metrics to the state of jax metrics

--- exciting_exciting_systems/related_work/np_reimpl/metrics.py
+++ exciting_exciting_systems/related_work/np_reimpl/metrics.py
@@ -6,6 +6,38 @@ This is necessary for a pure numpy implementation of the related work (which can
 import numpy as np
 
 
+def KLDLoss(p: np.ndarray, q: np.ndarray):
+    """Computes the sample KLD between two inputs.
+
+    The last dim of the input needs to be of length 1. The summation occurs along the second to
+    last dimension. All dimensions before that are kept as they are. Overall the shape of the
+    two inputs must be indentical.
+
+    TODO: add an eps=1e-32 to remove zero issues?
+    """
+    assert p.shape == q.shape, "The two inputs need to be of the same shape."
+    assert p.shape[-1] == q.shape[-1] == 1, "Last dim needs to be of length 1 for PDFs"
+
+    eps = 1e-12
+
+    kld = (p + eps) * np.log((p + eps) / (q + eps))
+    return np.squeeze(np.sum(kld, axis=-2))
+
+
+def JSDLoss(p: np.ndarray, q: np.ndarray):
+    """Computes the sample JSD between two inputs.
+
+    The last dim of the input needs to be of length 1. The summation occurs along the second to
+    last dimension. All dimensions before that are kept as they are. Overall the shape of the
+    two inputs must be indentical.
+    """
+    assert p.shape == q.shape, "The two inputs need to be of the same shape."
+    assert p.shape[-1] == q.shape[-1] == 1, "Last dim needs to be of length 1 for PDFs"
+
+    m = (p + q) / 2
+    return np.squeeze((KLDLoss(p, m) + KLDLoss(q, m)) / 2)
+
+
 def MNNS_without_penalty(data_points: np.ndarray, new_data_points: np.ndarray) -> np.ndarray:
     """From [Smits2024].
 
@@ -64,7 +96,7 @@ def MC_uniform_sampling_distribution_approximation(data_points: np.ndarray, supp
     return np.sum(minimal_distances) / M
 
 
-def audze_eglais(data_points: np.ndarray) -> np.ndarray:
+def audze_eglais(data_points: np.ndarray, eps: float = 0.001) -> np.ndarray:
     """From [Smits2024]. The maximin-desing penalizes points that
     are too close in the point distribution.
 
@@ -74,4 +106,69 @@ def audze_eglais(data_points: np.ndarray) -> np.ndarray:
     distance_matrix = np.linalg.norm(data_points[:, None, :] - data_points[None, ...], axis=-1)
     distances = distance_matrix[np.triu_indices(N, k=1)]
 
-    return 2 / (N * (N - 1)) * np.sum(1 / (distances**2 + 0.001))
+    return 2 / (N * (N - 1)) * np.sum(1 / (distances**2 + eps))
+
+
+def blockwise_mcudsa(data_points: np.ndarray, support_points: np.ndarray) -> np.ndarray:
+    M = support_points.shape[0]
+
+    block_size = 1_000
+    value = np.zeros(1)
+
+    for m in range(0, M, block_size):
+        end = min(m + block_size, M)  # next block or until the end
+        value = value + (
+            MC_uniform_sampling_distribution_approximation(
+                data_points=data_points,
+                support_points=support_points[m:end],
+            )
+            * (end - m)  # denormalizing mean inside loss computation
+            / M
+        )
+
+    return value
+
+
+def kiss_space_filling_cost(
+    data_points: np.ndarray,
+    support_points: np.ndarray,
+    variances: np.ndarray,
+    eps: float = 1e-16,
+) -> np.ndarray:
+    """From [Kiss2024]. Slightly modified to use the mean instead of the sum in the denominator.
+    The goal is to have the same metric value for identical data distributions with different number
+    of data points.
+    """
+    difference = data_points[None, ...] - support_points[:, None, :]
+    exponent = -0.5 * np.sum(difference**2 * 1 / variances, axis=-1)
+
+    denominator = eps + np.mean(np.exp(exponent), axis=-1)
+
+    return np.mean(1 / denominator, axis=0)
+
+
+def blockwise_ksfc(
+    data_points: np.ndarray,
+    support_points: np.ndarray,
+    variances: np.ndarray,
+    eps: float = 1e-16,
+) -> np.ndarray:
+    M = support_points.shape[0]
+
+    block_size = 1_000
+    value = np.zeros(1)
+
+    for m in range(0, M, block_size):
+        end = min(m + block_size, M)  # next block or until the end
+        value = value + (
+            kiss_space_filling_cost(
+                data_points=data_points,
+                support_points=support_points[m:end],
+                variances=variances,
+                eps=eps,
+            )
+            * (end - m)  # denormalizing mean inside loss computation
+            / M
+        )
+
+    return value
