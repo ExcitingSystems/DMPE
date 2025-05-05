9d437e2156c6a5b68e9071109ca9b7ca276aede2
Hendrik Vater <vater@lea.uni-paderborn.de>
2025-05-05 12:44:41 +0200

    added some minimal documentation to the signal generation

--- dmpe/utils/signals.py
+++ dmpe/utils/signals.py
@@ -5,13 +5,24 @@ import jax.numpy as jnp
 
 
 @partial(jax.jit, static_argnums=(0, 1))
-def generate_constant_action(n_steps, batch_size, key):
+def generate_constant_action(n_steps: int, batch_size: int, key: jax.random.PRNGKey) -> jax.Array:
+    """Randomly draws an action and repeats it for 'n_steps'."""
     actions = jax.random.uniform(key, shape=(batch_size, 1, 1), minval=-1, maxval=1)
     actions = jnp.repeat(actions, repeats=n_steps, axis=1)
     return actions
 
 
-def aprbs_single_batch(len, t_min, t_max, key):
+def aprbs_single_batch(len: int, t_min: float, t_max: float, key: jax.random.PRNGKey) -> jax.Array:
+    """Creates an amplitude modulated pseudorandom binary sequence (APRBS) in 1d and for 1 batch,
+    i.e. without a batch dimension.
+
+    Args:
+        len (int): Length of the signal.
+        t_min (float): Minimum hold time of an amplitude.
+        t_max (float): Maximum hold time of an amplitude
+        key (jax.random.PRNGKey): Random key for JAX random sampling.
+    """
+
     t = 0
     sig = []
     while t < len:
@@ -25,7 +36,17 @@ def aprbs_single_batch(len, t_min, t_max, key):
     return jnp.hstack(sig)[:len]
 
 
-def aprbs(n_steps, batch_size, t_min, t_max, key):
+def aprbs(n_steps: int, batch_size: int, t_min: float, t_max: float, key: jax.random.PRNGKey) -> jax.Array:
+    """Creates an amplitude modulated pseudorandom binary sequence (APRBS) in 1d where 'batch_size'
+    sequences are created independently next to each other.
+
+    Args:
+        n_steps (int): Length of the signal.
+        batch_size (int): Number of batches.
+        t_min (float): Minimum hold time of an amplitude.
+        t_max (float): Maximum hold time of an amplitude
+        key (jax.random.PRNGKey): Random key for JAX random sampling.
+    """
     actions = []
     for _ in range(batch_size):
         subkey, key = jax.random.split(key)
