fdece170b218791b31464eaa603fec2501a543e0
Hendrik Vater <hvater@mail.uni-paderborn.de>
2025-07-23 13:28:49 +0200

    updated dmpe_eval script to reflect setup changes

--- dmpe/evaluation/scripts/classical_systems/dmpe_eval.py
+++ dmpe/evaluation/scripts/classical_systems/dmpe_eval.py
@@ -22,6 +22,9 @@ from dmpe.excitation.excitation_utils import soft_penalty
 from dmpe.algorithms.algorithms import excite_with_dmpe
 from dmpe.models.models import NeuralEulerODEPendulum, NeuralEulerODE, NeuralEulerODECartpole
 from dmpe.models.model_utils import save_model
+from dmpe.utils.env_utils.fluid_tank_utils import setup_env as setup_fluid_tank_env
+from dmpe.utils.env_utils.pendulum_utils import setup_env as setup_pendulum_env
+from dmpe.utils.env_utils.cart_pole_utils import setup_env as setup_cart_pole_env
 
 
 # file path setup
@@ -61,17 +64,7 @@ if sys_name == "pendulum":
         feat_obs = jnp.stack([jnp.sin(obs[..., 0] * jnp.pi), jnp.cos(obs[..., 0] * jnp.pi), obs[..., 1]], axis=-1)
         return feat_obs
 
-    env_params = dict(batch_size=1, tau=2e-2, max_torque=5, g=9.81, l=1, m=1, env_solver=diffrax.Tsit5())
-    env = excenvs.make(
-        env_id="Pendulum-v0",
-        batch_size=env_params["batch_size"],
-        action_normalizations={
-            "torque": excenvs.utils.MinMaxNormalization(min=-env_params["max_torque"], max=env_params["max_torque"])
-        },
-        static_params={"g": env_params["g"], "l": env_params["l"], "m": env_params["m"]},
-        solver=env_params["env_solver"],
-        tau=env_params["tau"],
-    )
+    env, penalty_function, env_params = setup_pendulum_env()
     alg_params = dict(
         bandwidth=None,
         n_prediction_steps=20,
@@ -81,7 +74,7 @@ if sys_name == "pendulum":
         n_opt_steps=10,
         start_optimizing=5,
         consider_action_distribution=True,
-        penalty_function=None,
+        penalty_function=penalty_function,
         target_distribution=None,
         clip_action=True,
         n_starts=5,
@@ -97,10 +90,6 @@ if sys_name == "pendulum":
         )
     )
 
-    # overwrite penalty function and target distribution
-    alg_params["penalty_function"] = lambda x, u: soft_penalty(a=x, a_max=1, penalty_order=2) + soft_penalty(
-        a=u, a_max=1, penalty_order=2
-    )
     alg_params["target_distribution"] = get_uniform_target_distribution(
         dim=3 if alg_params["consider_action_distribution"] else 2,
         points_per_dim=alg_params["points_per_dim"],
@@ -118,7 +107,13 @@ if sys_name == "pendulum":
         featurize=featurize_theta,
         model_lr=1e-4,
     )
-    model_params = dict(obs_dim=env.physical_state_dim, action_dim=env.action_dim, width_size=128, depth=3, key=None)
+    model_params = dict(
+        obs_dim=env.physical_state_dim,
+        action_dim=env.action_dim,
+        width_size=128,
+        depth=3,
+        key=None,
+    )
 
     exp_params = dict(
         seed=None,
@@ -135,30 +130,7 @@ if sys_name == "pendulum":
 elif sys_name == "fluid_tank":
     ## Start fluid_tank experiment parameters
 
-    env_params = dict(
-        batch_size=1,
-        tau=5,
-        max_height=3,
-        max_inflow=0.2,
-        base_area=jnp.pi,
-        orifice_area=jnp.pi * 0.1**2,
-        c_d=0.6,
-        g=9.81,
-        env_solver=diffrax.Tsit5(),
-    )
-    env = excenvs.make(
-        "FluidTank-v0",
-        physical_normalizations=dict(height=excenvs.utils.MinMaxNormalization(min=0, max=env_params["max_height"])),
-        action_normalizations=dict(inflow=excenvs.utils.MinMaxNormalization(min=0, max=env_params["max_inflow"])),
-        static_params=dict(
-            base_area=env_params["base_area"],
-            orifice_area=env_params["orifice_area"],
-            c_d=env_params["c_d"],
-            g=env_params["g"],
-        ),
-        tau=env_params["tau"],
-        solver=env_params["env_solver"],
-    )
+    env, penalty_function, env_params = setup_fluid_tank_env()
 
     alg_params = dict(
         bandwidth=None,
@@ -169,7 +141,7 @@ elif sys_name == "fluid_tank":
         n_opt_steps=10,
         start_optimizing=5,
         consider_action_distribution=True,
-        penalty_function=None,
+        penalty_function=penalty_function,
         target_distribution=None,
         clip_action=True,
         n_starts=5,
@@ -186,9 +158,6 @@ elif sys_name == "fluid_tank":
     )
 
     # overwrite penalty function and target distribution
-    alg_params["penalty_function"] = lambda x, u: soft_penalty(a=x, a_max=1, penalty_order=2) + soft_penalty(
-        a=u, a_max=1, penalty_order=2
-    )
     alg_params["target_distribution"] = get_uniform_target_distribution(
         dim=2 if alg_params["consider_action_distribution"] else 1,
         points_per_dim=alg_params["points_per_dim"],
@@ -233,38 +202,7 @@ elif sys_name == "cart_pole":
         )
         return feat_obs
 
-    env_params = dict(
-        batch_size=1,
-        tau=2e-2,
-        max_force=10,
-        static_params={
-            "mu_p": 0.002,
-            "mu_c": 0.5,
-            "l": 0.5,
-            "m_p": 0.1,
-            "m_c": 1,
-            "g": 9.81,
-        },
-        physical_normalizations={
-            "deflection": excenvs.utils.MinMaxNormalization(min=-2.4, max=2.4),
-            "velocity": excenvs.utils.MinMaxNormalization(min=-8, max=8),
-            "theta": excenvs.utils.MinMaxNormalization(min=-jnp.pi, max=jnp.pi),
-            "omega": excenvs.utils.MinMaxNormalization(min=-8, max=8),
-        },
-        env_solver=diffrax.Tsit5(),
-    )
-    env = excenvs.make(
-        env_id="CartPole-v0",
-        batch_size=env_params["batch_size"],
-        action_normalizations={
-            "force": excenvs.utils.MinMaxNormalization(min=-env_params["max_force"], max=env_params["max_force"])
-        },
-        physical_normalizations=env_params["physical_normalizations"],
-        static_params=env_params["static_params"],
-        solver=env_params["env_solver"],
-        tau=env_params["tau"],
-    )
-
+    env, penalty_function, env_params = setup_cart_pole_env()
     alg_params = dict(
         bandwidth=0.12,
         n_prediction_steps=50,
@@ -274,7 +212,7 @@ elif sys_name == "cart_pole":
         n_opt_steps=5,
         start_optimizing=5,
         consider_action_distribution=True,
-        penalty_function=None,
+        penalty_function=penalty_function,
         target_distribution=None,
         clip_action=True,
         n_starts=5,
@@ -291,9 +229,6 @@ elif sys_name == "cart_pole":
     # )
 
     # overwrite penalty function and target distribution
-    alg_params["penalty_function"] = lambda x, u: soft_penalty(a=x, a_max=1, penalty_order=2) + soft_penalty(
-        a=u, a_max=1, penalty_order=2
-    )
     alg_params["target_distribution"] = get_uniform_target_distribution(
         dim=5 if alg_params["consider_action_distribution"] else 4,
         points_per_dim=alg_params["points_per_dim"],
