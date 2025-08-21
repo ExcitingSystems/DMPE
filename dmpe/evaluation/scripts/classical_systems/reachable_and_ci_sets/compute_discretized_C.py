099885d81e943d6a2e46e6cffc6fa45553d9614e
Hendrik Vater <hvater@mail.uni-paderborn.de>
2025-08-21 11:04:03 +0200

    added initial parameterization for pendulum and ft set computations

--- dmpe/evaluation/scripts/classical_systems/reachable_and_ci_sets/compute_discretized_C.py
+++ dmpe/evaluation/scripts/classical_systems/reachable_and_ci_sets/compute_discretized_C.py
@@ -8,6 +8,8 @@ import jax
 import jax.numpy as jnp
 
 from dmpe.data_management import DataPaths
+from dmpe.utils.env_utils.fluid_tank_utils import setup_env as setup_fluid_tank_env
+from dmpe.utils.env_utils.pendulum_utils import setup_env as setup_pendulum_env
 from dmpe.utils.env_utils.cart_pole_utils import setup_env as setup_cart_pole_env
 from dmpe.utils.sets.control_invariant_set import approximate_control_invariant_set_through_chunks
 from dmpe.utils.sets.shared import save_discretized_set
@@ -30,9 +32,46 @@ gpu_id = args.gpu_id
 jax.config.update("jax_default_device", gpus[args.gpu_id])
 
 if sys_name == "fluid_tank":
-    raise NotImplementedError()
+    env, penalty_function, featurize, _ = setup_fluid_tank_env()
+    key = jax.random.PRNGKey(14)
+
+    obs_dim = env.reset(env.env_properties)[0].shape[-1]
+    sequence_length = 200
+    n_starts = 10
+    n_opt_steps = 1_000
+    points_per_dim = 50
+    chunk_size = 20_000
+
+    xs = [
+        jnp.linspace(-1.0, 1.0, points_per_dim),
+        jnp.linspace(-1.0, 1.0, points_per_dim),
+    ]
+    z_g = jnp.meshgrid(*xs, indexing="ij")
+    z_g = jnp.stack([_x for _x in z_g], axis=-1)
+    unflattened_shape = z_g.shape[:-1]
+    init_observations = z_g.reshape(-1, obs_dim)
+
 elif sys_name == "pendulum":
-    raise NotImplementedError()
+    env, penalty_function, featurize, _ = setup_pendulum_env()
+    key = jax.random.PRNGKey(14)
+
+    obs_dim = env.reset(env.env_properties)[0].shape[-1]
+    sequence_length = 200
+    n_starts = 10
+    n_opt_steps = 1_000
+    points_per_dim = 50
+    chunk_size = 20_000
+
+    xs = [
+        jnp.linspace(-1.0, 1.0, points_per_dim),
+        jnp.linspace(-1.0, 1.0, points_per_dim),
+        jnp.linspace(-1.0, 1.0, points_per_dim),
+    ]
+    z_g = jnp.meshgrid(*xs, indexing="ij")
+    z_g = jnp.stack([_x for _x in z_g], axis=-1)
+    unflattened_shape = z_g.shape[:-1]
+    init_observations = z_g.reshape(-1, obs_dim)
+
 elif sys_name == "cart_pole":
     env, penalty_function, featurize, _ = setup_cart_pole_env()
     key = jax.random.PRNGKey(14)
@@ -53,7 +92,6 @@ elif sys_name == "cart_pole":
     z_g = jnp.meshgrid(*xs, indexing="ij")
     z_g = jnp.stack([_x for _x in z_g], axis=-1)
     unflattened_shape = z_g.shape[:-1]
-
     init_observations = z_g.reshape(-1, obs_dim)
 
 C = approximate_control_invariant_set_through_chunks(
